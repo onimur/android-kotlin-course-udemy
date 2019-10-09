7273e65d030e91ca6c83eefedc0df2337b519b28
Murillo Comino <murillo_comino@hotmail.com>
2019-10-09 12:33:39 -0300

    Section 20 (87) - Identifying elements

--- app/src/main/java/com/onimus/courseandroidkotlinudemy/MainActivity.kt
+++ app/src/main/java/com/onimus/courseandroidkotlinudemy/MainActivity.kt
@@ -2,11 +2,16 @@ package com.onimus.courseandroidkotlinudemy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //var etDistance = findViewById<EditText>(R.id.etDistance)
+        etDistance
     }
 }

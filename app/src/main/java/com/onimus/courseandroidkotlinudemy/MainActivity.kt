de593012a677bb621ef3b011a25c90e0f1f25f93
Murillo Comino <murillo_comino@hotmail.com>
2019-10-09 12:42:26 -0300

    Section 20 (88) - Adding events to elements

--- app/src/main/java/com/onimus/courseandroidkotlinudemy/MainActivity.kt
+++ app/src/main/java/com/onimus/courseandroidkotlinudemy/MainActivity.kt
@@ -2,16 +2,28 @@ package com.onimus.courseandroidkotlinudemy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(v: View) {
+        if (v.id == R.id.btnCalculate){
+            handleCalculate()
+        }
+    }
+
+    private fun handleCalculate() {
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //var etDistance = findViewById<EditText>(R.id.etDistance)
-        etDistance
+        //etDistance
+
+        btnCalculate.setOnClickListener(this)
     }
 }

ebdd6dfbe94cbfc16cef230dcae19dd5e04d47d4
Murillo Comino <murillo_comino@hotmail.com>
2019-10-10 20:13:19 -0300

    Section 24 (134) Customizing Toast Notification

--- app/src/main/java/com/onimus/courseandroidkotlinudemy/components/ComponentsActivity.kt
+++ app/src/main/java/com/onimus/courseandroidkotlinudemy/components/ComponentsActivity.kt
@@ -1,9 +1,11 @@
 package com.onimus.courseandroidkotlinudemy.components
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import com.onimus.courseandroidkotlinudemy.MainActivity
 import com.onimus.courseandroidkotlinudemy.R
@@ -19,7 +21,20 @@ class ComponentsActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View) {
         if (v.id == R.id.btnToastMe) {
-            Toast.makeText(this, "Toast Notification!", Toast.LENGTH_LONG).show()
+            //val toast =  Toast.makeText(this, "Toast Notification!", Toast.LENGTH_LONG)
+            val toast = Toast(this)
+            //Layout
+            val toastLayout = layoutInflater.inflate(R.layout.toast_custom, null)
+            with(toast) {
+                view = toastLayout
+                val textView = view.findViewById<TextView>(R.id.tvMessage)
+                with(textView) {
+                    setTextColor(Color.RED)
+                    text = ("Toast Notification")
+                }
+                duration = Toast.LENGTH_LONG
+                show()
+            }
         }
     }
 

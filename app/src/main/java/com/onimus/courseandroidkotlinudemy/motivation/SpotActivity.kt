6a6fb99a2fc9819525820ca24f72a93a82043cd4
Murillo Comino <murillo_comino@hotmail.com>
2019-10-10 13:45:54 -0300

    Section 23 (123) - Using constraint layout I

---
+++ app/src/main/java/com/onimus/courseandroidkotlinudemy/motivation/SpotActivity.kt
@@ -0,0 +1,13 @@
+package com.onimus.courseandroidkotlinudemy.motivation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.onimus.courseandroidkotlinudemy.R
+
+class SpotActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spot)
+    }
+}
